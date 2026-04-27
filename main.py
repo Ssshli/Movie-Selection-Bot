b24125b9ebc9411c7369c08bb995b1d12f1201ca
Ssshli <goodrhroome34@gmail.com>
2026-04-27 20:15:10 +0000

    Доработки + цикл while

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import random
 from movies_data import all_movies
-genre=input("Какой  жанр фильма вы хотите посмотреть?").lower()
-country=input("Выберите страну из предложенных:Россия,США,Франция,Германия,.Прошу заметить, что фильмы представлены на платформе Кинопоиск, поиск фильмов ведется оттуда.")
-if genre in all_movies and country in all_movies[genre]:
-  film=random.choice(all_movies[genre][country])
-  print(f"Приятного просмотра!:{film}")
-else:
-  print("Извините! Я не знаю такого жанра/фильма, попробуйте ввести действительный жанр и название страны с заглавной буквой")
+while True:
+  genre=input("Какой  жанр фильма вы хотите посмотреть?").lower()
+  country=input("Выберите страну из предложенных:Россия,США,Франция,Германия,.Прошу заметить, что фильмы представлены на платформе Кинопоиск, поиск фильмов ведется оттуда.")
+  if genre in all_movies and country in all_movies[genre]:
+    film=random.choice(all_movies[genre][country])
+    print(f"Приятного просмотра!:{film}")
+  else:
+    print("Извините! Я не знаю такого жанра/фильма, попробуйте ввести действительный жанр и название страны с заглавной буквой")
+  question=input("Хотите выбрать еще один фильм?").strip()
+  if question == "нет":
+    print("Хорошо! Обращайтесь за помощью!")
+    break
