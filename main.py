8938a08a4242b4211ffcdbbd9c20464619f6b620
Ssshli <nananaea8@mail.ru>
2026-05-01 16:29:21 +0400

    Изменение приветствия

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import random
 from movies_data import all_movies
 while True:
-  genre=input("Какой  жанр фильма вы хотите посмотреть?").lower()
+  genre=input("Здравствуйте! Какой  жанр фильма вы хотите посмотреть?").lower()
   country=input("Выберите страну из предложенных:Россия,США,Франция,Германия,.Прошу заметить, что фильмы представлены на платформе Кинопоиск, поиск фильмов ведется оттуда.")
   if genre in all_movies and country in all_movies[genre]:
     film=random.choice(all_movies[genre][country])
